f1d4cb2a3e2bcb3337d6f5c7352a991f012b6ccf
Sebastien Desilets <solantd@hotmail.com>
2013-11-24 06:31:12 -0500

    Added Parallax

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -8,6 +8,13 @@ public class PlayerScript : MonoBehaviour {
     /// </summary>
     public Vector2 speed = new Vector2(50, 50);
 
+    private WeaponScript weapon;
+
+    void Awake() {
+        // Retrieve the weapon only once
+        weapon = GetComponentInChildren<WeaponScript>();
+    }
+
     void Update() {
 
         // 2 - Retrieve axis information
@@ -31,11 +38,34 @@ public class PlayerScript : MonoBehaviour {
         shoot |= Input.GetButton("Fire2");
 
         if (shoot) {
-            WeaponScript weapon = GetComponent<WeaponScript>();
             if (weapon != null) {
                 // false because the player is not an enemy
                 weapon.Attack(false);
             }
         }
+
+        var dist = (transform.position - Camera.main.transform.position).z;
+
+        var leftBorder = Camera.main.ViewportToWorldPoint(
+          new Vector3(0, 0, dist)
+        ).x;
+
+        var rightBorder = Camera.main.ViewportToWorldPoint(
+          new Vector3(1, 0, dist)
+        ).x;
+
+        var topBorder = Camera.main.ViewportToWorldPoint(
+          new Vector3(0, 0, dist)
+        ).y;
+
+        var bottomBorder = Camera.main.ViewportToWorldPoint(
+          new Vector3(0, 1, dist)
+        ).y;
+
+        transform.position = new Vector3(
+          Mathf.Clamp(transform.position.x, leftBorder, rightBorder),
+          Mathf.Clamp(transform.position.y, topBorder, bottomBorder),
+          transform.position.z
+        );
     }
 }
